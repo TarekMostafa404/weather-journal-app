3df3598dcddd789ee2236d28f4b3a6fdf6587480
Tarek Mostafa <tarekmostafa.dev@gmail.com>
2021-04-03 00:03:10 +0200

    server completed

--- server.js
+++ server.js
@@ -2,18 +2,35 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
+const port = 8888;
 // Setup Server
+const server = app.listen(port, function() {
+    console.log(`server running in port${port}`);
+})
+
+app.get('/data', function(req, res) {
+    res.send(projectData);
+})
+
+app.post('/addData', function(req, res) {
+    projectData.temp = req.body.temp;
+    projectData.date = req.body.date;
+    projectData.userValue = req.body.feelings;
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 /* Global Variables */
-
+const apiKey = '8f155260d94c860f85dd5c1245283323';
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
